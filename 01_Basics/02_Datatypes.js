8a39f8a9b29da9910afd9d14c09c0be8bd43d10f
Ravinder kumar <117176922+ravi1957@users.noreply.github.com>
2023-09-27 06:05:24 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all js code in newer version
+
+// alert(3 + 3) // We are using nodejs, not browser
+
+// console.log(3 + 3);
+
+// console.log("Ravi");
+
+// Datatypes in JS
+
+// let name = "Ravi";
+// let age = 23;
+// let isLoggedIn = false;
+
+/*
+==========================
+Primitive Datatypes
+==========================
+*/
+
+// number => 2 to power 53
+// bigint
+// String = ""
+// Boolean = True/false
+// null => standalone value
+// undefined => not assigned the value
+// symbol => unique
+
+/*
+===================================
+Refrence Datatype
+===================================
+ // Object
+ // Arrays
+*/
+
+
+console.log(typeof "ravi");  // string
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
