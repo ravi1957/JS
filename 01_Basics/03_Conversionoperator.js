827a2f8788689e151fdbf01b02f2a7904261d740
Ravinder kumar <117176922+ravi1957@users.noreply.github.com>
2023-09-27 06:31:05 +0000

    conversion

---
+++ 01_Basics/03_Conversionoperator.js
@@ -0,0 +1,30 @@
+// let age = "33ass";
+// let age = null;
+// let age = undefined;
+// let age = true;
+
+// console.log(typeof age);
+// console.log(typeof (age));
+
+// let inNum = Number(age)
+// console.log(typeof (inNum));
+// console.log(inNum);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1;
+let isLoggedIn = "ravi";
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log((booleanIsLoggedIn));
+
+
+let num = 44;
+console.log(typeof num);
+let stringnum = String(num)
+console.log(typeof stringnum);
